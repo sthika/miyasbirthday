feb26ed3db10a6de9c1486f6656dbfb86161155b
sthika <sveta.tyo45@gmail.com>
2023-09-30 13:29:36 +0600

    updated CV

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -7,7 +7,7 @@ const Skills = () => {
   return (
     <section className="skills section" id='skills'>
         <h2 className="section__title">Skills</h2>
-        <span className="section__subtitle">My texh lvl</span>
+        <span className="section__subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
 
         <div className="skills__container container grid">
             <Frontend/>
